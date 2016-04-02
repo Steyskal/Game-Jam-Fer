afe25133f7b6d3ec511090890cc02393a8f2cbec
gradecak ivan <ch13ft41n@yahoo.com>
2016-04-02 14:34:16 +0200

    controle...

--- Assets/Scripts/Controle.cs
+++ Assets/Scripts/Controle.cs
@@ -3,13 +3,60 @@ using System.Collections;
 
 public class Controle : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    Rigidbody playerRigidbody;
+    Vector3 movement;
+    Vector3 rotation;
+
+    private float moveH;
+    private float moveV;
+    private float moveLT_RT;
+
+    public float moveSpeed;
+    public float rotSpeed;
+
+    // Joystick1Button4 LB
+    // Joystick1Button5 RB
+    // Joystick1Button9 donja gljivica kad se stisne
+    // Joystick1Button8 gornja gljivica kad se stisne
+    // Joystick1Button7 start butt
+    // Joystick1Button6 stop butt
+
+    void Start () {
+
+        playerRigidbody = GetComponent<Rigidbody>();
+
+    }
 	
-	}
-	
-	// Update is called once per frame
 	void Update () {
-	
-	}
+        
+        moveV = Input.GetAxis("Horizontal");
+        moveH = Input.GetAxis("Vertical");
+
+        moveLT_RT = Input.GetAxis("LTRT");
+
+        
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+        Rotate();
+        
+    }
+
+    void Move()
+    {
+        playerRigidbody.MovePosition(transform.position + transform.forward * (moveSpeed / 10) * moveLT_RT * -1);
+        
+    }
+
+    void Rotate()
+    {
+        rotation.Set(moveH * rotSpeed, moveV * rotSpeed, 0.0f);
+                
+        Quaternion deltaRotation = Quaternion.Euler(rotation * Time.deltaTime);
+        playerRigidbody.MoveRotation(playerRigidbody.rotation * deltaRotation);
+
+    }
+
 }
